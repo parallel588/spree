2321ef72aaa6872fe4d80f77eb650e4ab8cb0f42
Maxim Pechnikov <parallel588@gmail.com>
2014-01-27 11:40:12 +0300

    fixed order validate for attachment

--- core/app/models/spree/image.rb
+++ core/app/models/spree/image.rb
@@ -1,8 +1,6 @@
 module Spree
   class Image < Asset
-    validates_attachment_presence :attachment
-    validate :no_attachment_errors
-
+    
     attr_accessible :alt, :attachment, :position, :viewable_type, :viewable_id
 
     has_attached_file :attachment,
@@ -11,7 +9,8 @@ module Spree
                       url: '/spree/products/:id/:style/:basename.:extension',
                       path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
                       convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
-
+    validates_attachment_presence :attachment
+    validate :no_attachment_errors
     # save the w,h of the original image (from which others can be calculated)
     # we need to look at the write-queue for images which have not been saved yet
     after_post_process :find_dimensions
